0c904699e574840a90024da2c0470a9d9bc1da8d
josuezolakiomd <josuezolakiocanal@gmail.com>
2024-05-27 13:33:52 -0400

    Project completed

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import mustache from 'mustache-express';
 import path from 'path';
+import * as mainRoutes from './routes/index';
 
 // Loading the environment variables
 dotenv.config();
@@ -18,6 +19,12 @@ server.engine('mustache', mustache());
 server.use(express.static(path.join(__dirname, '../public')));
 
 // Setting up the routes
+server.use(mainRoutes.default);
+
+server.use((req: Request, res: Response) => {
+  res.render('pages/404');
+})
+
 
 // Starting the server
 server.listen(process.env.PORT, () => {
